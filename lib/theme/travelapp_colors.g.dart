6afc97320725660516148717db16ff2cc22587c4
Parabeac-Nest <admin@parabeac.com>
2022-11-14 21:34:02 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/travelapp_colors.g.dart
@@ -0,0 +1,78 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class TravelappColors {
+
+static const Color iconColor = Color(0xff525252);
+/// Used in CTA, H1
+static const Color primaryColor = Color(0xff034c4c);
+
+static const Color secondaryColor = Color(0xff8ac6be);
+/// token=md.sys.color.secondary-container
+
+secondary-container / on-secondary-container
+
+Regular text:
+Level AA - ✅ Pass
+Level AAA - ✅ Pass
+
+Large text:
+Level AA - ✅ Pass
+Level AAA - ✅ Pass
+
+Icons:
+Level AA - ✅ Pass
+
+static const Color m3SysLightSecondaryContainer = Color(0xffe8def8);
+
+static const Color subtitleColor = Color(0xff201f1f);
+
+static const Color accentColor = Color(0xffc5580a);
+
+static const Color percentageEmpty = Color(0xffb0bcbb);
+
+static const Color backgroundLight = Color(0xfffaf7f3);
+
+static const Color textColor4F = Color(0xff4f4f4f);
+
+static const Color textColor = Color(0xff333232);
+
+static const Color backgroundDark = Color(0xff1c1b1f);
+
+static const Color errorTextColor = Color(0xffae020e);
+
+static const Color errorBgColor = Color(0xfff6cabc);
+
+static const Color sucessTextColor = Color(0xff007a57);
+
+static const Color sucessBgColor = Color(0xffe4fde0);
+
+static const Color warningTextColor = Color(0xffac4a04);
+
+static const Color warningBgColor = Color(0xfffdf1c9);
+
+static const Color infoTextColor = Color(0xff1948ac);
+
+static const Color infoBgColor = Color(0xffc0e1ff);
+/// token=md.sys.color.primary
+
+primary / on-primary
+
+Regular text:
+Level AA - ✅ Pass
+Level AAA - ❌ Fail
+
+Large text:
+Level AA - ✅ Pass
+Level AAA - ✅ Pass
+
+Icons:
+Level AA - ✅ Pass
+
+static const Color m3SysLightPrimary = Color(0xff6750a4);
+}
